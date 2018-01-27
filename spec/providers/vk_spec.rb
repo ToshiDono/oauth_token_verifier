8ced04c65cf24ea4116f8cb5bf5b9f67392a77a1
Anton <toshidono.it.work@gmail.com>
2018-01-27 19:06:56 +0300

    Add vcr

--- spec/providers/vk_spec.rb
+++ spec/providers/vk_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 require "shared_examples/oauth_provider_client"
-require 'support/webmock_stub_helper'
 
 RSpec.describe 'OauthTokenVerifier::Providers::Vk' do
   describe '#verify_token' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,20 @@
 require 'bundler/setup'
 require 'oauth_token_verifier'
-require 'webmock/rspec'
+require 'webmock'
+require 'vcr'
+require_relative 'support/vcr_setup'
 
 WebMock.disable_net_connect!(allow_localhost: true)
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+  end
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -13,5 +25,11 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.around(:each, :vcr) do |example|
+    name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
+    VCR.use_cassette(name) { example.call }
   end
 end
